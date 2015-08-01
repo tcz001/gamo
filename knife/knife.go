87518186522c195742315d5afa1c2e553b9ddaca
Fan Jiang <fanjiang@thoughtworks.com>
2015-08-01 17:02:09 -0500

    add start server

--- knife/knife.go
+++ knife/knife.go
@@ -1,12 +1,14 @@
 package knife
 
 import (
-	"fmt"
 	"net"
+
+	"google.golang.org/grpc"
 )
 
 //Server is the game Server instance
 type Server struct {
+	*grpc.Server
 	port string
 	lis  net.Listener
 }
@@ -14,13 +16,26 @@ type Server struct {
 const _port = ":50000"
 
 //Init is to initialize a Serve instance
-func (s *Server) Init() error {
-	lis, err := net.Listen("tcp", _port)
+func (s *Server) Init(port string) {
+	if port != "" {
+		s.port = port
+	} else {
+		s.port = _port
+	}
+	s.Server = grpc.NewServer()
+}
+
+//Stop is to start a Serve instance
+func (s *Server) Stop() error {
+	return nil
+}
+
+//Start is to start a Serve instance
+func (s *Server) Start() error {
+	lis, err := net.Listen("tcp", s.port)
 	if err != nil {
 		return err
 	}
-	fmt.Println("listening on ", lis.Addr())
-	s.port = _port
 	s.lis = lis
 	return nil
 }
--- knife/knife_test.go
+++ knife/knife_test.go
@@ -14,8 +14,21 @@ var _ = Suite(&KnifeSuite{})
 
 func (s *KnifeSuite) TestServerInit(c *C) {
 	var server Server
-	err := server.Init()
-	c.Assert(err, IsNil)
+	server.Init("")
 	c.Assert(server.port, Equals, _port)
-	c.Assert(server.lis, NotNil)
+}
+
+func (s *KnifeSuite) TestServerStart(c *C) {
+	var dup Server
+	err := dup.Start()
+	c.Assert(err, IsNil)
+	c.Assert(dup.lis, NotNil)
+}
+
+func (s *KnifeSuite) TestServerStartError(c *C) {
+	var dup Server
+	dup.Init(":invalid port")
+	err := dup.Start()
+	c.Assert(err, NotNil)
+	c.Assert(dup.lis, IsNil)
 }

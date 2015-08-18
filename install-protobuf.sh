6388182c2781f9e3f2dce53639471b5a5b1a43e3
Fan Jiang <fanjiang@thoughtworks.com>
2015-08-18 17:52:25 -0500

    try use prefix for protobuf

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
 git clone https://github.com/google/protobuf.git
-cd protobuf && ./autogen.sh && ./configure && make && make check && make install
+cd protobuf && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make check && make install

bd013560cafba1da056dc660cea13d026d4bd6dc
Fan Jiang <fanjiang@thoughtworks.com>
2015-08-18 17:55:23 -0500

    use cache in travis

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-set -ex
-git clone https://github.com/google/protobuf.git
-cd protobuf && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make check && make install
+set -e
+# check to see if protobuf folder is empty
+if [ ! -d "$HOME/protobuf/lib" ]; then
+    git clone https://github.com/google/protobuf.git
+    cd protobuf && ./autogen.sh && ./configure --prefix=$HOME/protobuf && make && make check && make install
+else
+    echo 'Using cached directory.';
+fi
